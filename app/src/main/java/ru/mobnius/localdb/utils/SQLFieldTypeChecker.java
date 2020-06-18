e62bc30812814e9468b5c47712e650c0b3a738d0
Sandroisu <sandroisu1411@gmail.com>
2020-06-18 13:39:23 +0300

    minor

--- app/src/main/java/ru/mobnius/localdb/utils/SQLFieldTypeChecker.java
+++ app/src/main/java/ru/mobnius/localdb/utils/SQLFieldTypeChecker.java
@@ -3,11 +3,11 @@ package ru.mobnius.localdb.utils;
 import android.database.Cursor;
 
 public class SQLFieldTypeChecker {
-    static final int FIELD_TYPE_NULL = 0;
-    static final int FIELD_TYPE_INTEGER = 1;
-    static final int FIELD_TYPE_FLOAT = 2;
-    static final int FIELD_TYPE_STRING = 3;
-    static final int FIELD_TYPE_BLOB = 4;
+    private static final int FIELD_TYPE_NULL = 0;
+    private static final int FIELD_TYPE_INTEGER = 1;
+    private static final int FIELD_TYPE_FLOAT = 2;
+    private static final int FIELD_TYPE_STRING = 3;
+    private static final int FIELD_TYPE_BLOB = 4;
 
     public static Object getType(Cursor cursor, int columnIndex) {
         switch (cursor.getType(columnIndex)) {
