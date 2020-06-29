2f7fc793882dc54c08e64c23fb9e8c877092ad64
Sandroisu <sandroisu1411@gmail.com>
2020-06-29 10:42:20 +0300

    initial commit

--- app/src/main/java/ru/mobnius/localdb/data/ExceptionInterceptActivity.java
+++ app/src/main/java/ru/mobnius/localdb/data/ExceptionInterceptActivity.java
@@ -2,9 +2,13 @@ package ru.mobnius.localdb.data;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+
 import ru.mobnius.localdb.data.exception.ExceptionGroup;
+import ru.mobnius.localdb.data.exception.FileExceptionManager;
 import ru.mobnius.localdb.data.exception.OnExceptionIntercept;
 import ru.mobnius.localdb.data.exception.MyUncaughtExceptionHandler;
+import ru.mobnius.localdb.ui.ExceptionActivity;
 
 /**
  * абстрактный класс для реализации обработчиков ошибок
@@ -13,6 +17,19 @@ public abstract class ExceptionInterceptActivity extends AppCompatActivity imple
 
     public void onExceptionIntercept() {
         Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler(Thread.getDefaultUncaughtExceptionHandler(), getExceptionGroup(), getExceptionCode(), this));
+        File root = FileExceptionManager.getInstance(this).getRootCatalog();
+        String[] files = root.list();
+        if (files != null) {
+            for (String fileName : files) {
+                byte[] bytes = FileExceptionManager.getInstance(this).readPath(fileName);
+                if (bytes != null) {
+                    finish();
+                    String message = new String(bytes);
+                    startActivity(ExceptionActivity.getExceptionActivityIntent(this, message));
+                }
+            }
+        }
+
     }
 
     @Override
